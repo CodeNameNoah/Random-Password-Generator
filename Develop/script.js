d1393335222d222b19b05b549f46f7d75b2de055
Noah Hoang <hoincbusiness@gmail.com>
2023-03-30 20:51:54 -0700

    Reformatted repository

---
+++ Develop/script.js
@@ -0,0 +1,16 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// Create an object to store password properties for requirements
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
